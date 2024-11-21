81e8ee6ee3a63404991d63bf1ba9c2dd8125026b
Marchant01 <celvin.marchant@hotmail.com>
2024-11-21 17:40:28 +0100

    Uppdated program.cs
    Uppdated program.cs file. Previously there was no ssh key on the local
    machine thus the changes haven't been uploaded to github.

--- app/Program.cs
+++ app/Program.cs
@@ -1,27 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
 // hangman
 
 string[] possible_words = ["new", "console", "template", "https", "information"];
 Random random_numbers = new();
 
 string solution = possible_words[random_numbers.Next(possible_words_words.Length)];
+string display = new('_', solution.Length);
 
-char guess;
-string? input;
+Console.WriteLine(display);
 
-do
-{
-    Console.WriteLine("Please enter a character to guess with")
-    input = Console.Readline();
-    Debug.Assert(input != null);
-} while (input.Length != 1)
-    
+char guess = get_user_guess();
 bool guess_was_correct = false;
 
-foreach (char c in solution)
+for (int i = 0; i < solution.Length; i++)
 {
-    if (guess == c)
+    if (gues[0]== solution[i])
     {
+        display.Remove(i,1)
+        display.Insert(i, guess)
         guess_was_correct = true;
     }
 }
@@ -33,4 +30,19 @@ if (guess_was_correct)
 else
 {
     Console.WriteLine($"{guess} doesn't exists in the word");
+}
+
+string get_user_guess()
+{
+    string ? guess == null;
+
+    do
+    {
+        Console.WriteLine("Please enter a character to guess with")
+        string input = Console.Readline();
+        Debug.Assert(input != null);
+        if (input.Length == 1)
+            guess = input[0];
+    } while (guess == null);
+    return guess;
 }
